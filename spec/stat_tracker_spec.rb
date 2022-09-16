db5277a85b9da612b65081e9f2e631f7d5a12e8e
Aether Kerstens <philippekerstens@gmail.com>
2022-09-15 17:23:46 -0700

    Write text description skeleton

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,4 +1,4 @@
-require './spec_helper.rb'
+require 'spec_helper.rb'
 require './lib/stat_tracker.rb'
 require 'csv'
 
@@ -80,7 +80,7 @@ RSpec.describe StatTracker do
 
     it 'highest_total_score method, sum of winning and losing team scores' do
       expect(@stat_tracker.highest_total_score).to eq 5
-   
+
     end
 
     it 'lowest_total_score, lowest sum of winning and losing teams scores' do
@@ -88,12 +88,12 @@ RSpec.describe StatTracker do
 
     end
 
-    it 'lowest_scoring_visitor, name of team with lowest avg score per game' do 
+    it 'lowest_scoring_visitor, name of team with lowest avg score per game' do
 
       expect(@stat_tracker.lowest_scoring_visitor).to eq("Sporting Kansas City")
     end
 
-    it 'lowest_scoring_home, name of team with lowest average score while at home' do 
+    it 'lowest_scoring_home, name of team with lowest average score while at home' do
       expect(@stat_tracker.lowest_scoring_home_team).to eq("Sporting Kansas City")
 
     end
@@ -121,8 +121,8 @@ RSpec.describe StatTracker do
     end
   end
 
-  describe "winningest and worst coaches methods" do 
-    before(:each) do 
+  describe "winningest and worst coaches methods" do
+    before(:each) do
       dummy_game_path = './data/dummy_games.csv'
       dummy_team_path = './data/dummy_teams.csv'
       dummy_game_teams_path = './data/dummy_game_teams.csv'
@@ -133,42 +133,42 @@ RSpec.describe StatTracker do
       }
       @stat_tracker = StatTracker.from_csv(locations)
     end
-  
-    it "has games_by_season which returns an array of games_ids" do 
+
+    it "has games_by_season which returns an array of games_ids" do
       expect(@stat_tracker.games_by_season("20122013")).to eq ["2012030221", "2012030222", "2012030223", "2012030224", "2012030225", "2012030311", "2012030312", "2012030313", "2012030314"]
     end
-  
-    it "has data_by_season which returns an array of game_teams_data rows" do 
+
+    it "has data_by_season which returns an array of game_teams_data rows" do
       expect(@stat_tracker.data_by_season("20122013")).to be_an Array
       coaches = @stat_tracker.data_by_season("20122013").map { |row| row["head_coach"] }
       expect(coaches).to eq ["John Tortorella", "Claude Julien", "John Tortorella", "Claude Julien", "Claude Julien", "John Tortorella", "Claude Julien", "John Tortorella", "John Tortorella"]
     end
-  
-    it "has wins_by_coach which returns hash with coach key and no. wins value" do 
+
+    it "has wins_by_coach which returns hash with coach key and no. wins value" do
       expect(@stat_tracker.wins_by_coach("20122013")).to be_a Hash
       expect(@stat_tracker.wins_by_coach("20122013")["John Tortorella"]).to eq 0
       expect(@stat_tracker.wins_by_coach("20122013")["Claude Julien"]).to eq 4
     end
-  
-    it "has sorted_wins_by_coach which returns a nested array sorting wins_by_coach by wins" do 
+
+    it "has sorted_wins_by_coach which returns a nested array sorting wins_by_coach by wins" do
       expect(@stat_tracker.sorted_wins_by_coach("20122013")).to be_an Array
-      max_wins = @stat_tracker.wins_by_coach("20122013").values.max 
-      min_wins = @stat_tracker.wins_by_coach("20122013").values.min 
+      max_wins = @stat_tracker.wins_by_coach("20122013").values.max
+      min_wins = @stat_tracker.wins_by_coach("20122013").values.min
       expect(@stat_tracker.sorted_wins_by_coach("20122013")[0][1]).to eq max_wins
       expect(@stat_tracker.sorted_wins_by_coach("20122013")[-1][1]).to eq min_wins
     end
-  
-    it "has winningest_coach which returns the name of winningest coach as a string" do 
+
+    it "has winningest_coach which returns the name of winningest coach as a string" do
       expect(@stat_tracker.winningest_coach("20122013")).to eq "Claude Julien"
     end
-  
-    it "has worst_coach which returns the name of the worst coach as a string" do 
+
+    it "has worst_coach which returns the name of the worst coach as a string" do
       expect(@stat_tracker.worst_coach("20122013")).to eq "John Tortorella"
     end
   end
 
-  describe "best and worst teams methods" do 
-    before(:each) do 
+  describe "best and worst teams methods" do
+    before(:each) do
       dummy_game_path = './data/dummy_games.csv'
       dummy_team_path = './data/dummy_teams.csv'
       dummy_game_teams_path = './data/dummy_game_teams.csv'
@@ -178,9 +178,9 @@ RSpec.describe StatTracker do
         game_teams: dummy_game_teams_path
       }
       @stat_tracker = StatTracker.from_csv(locations)
-    end 
+    end
 
-    it "has a season_ids method which returns an array of all season_ids" do 
+    it "has a season_ids method which returns an array of all season_ids" do
       expect(@stat_tracker.season_ids).to eq ["20122013"]
     end
 
@@ -188,16 +188,16 @@ RSpec.describe StatTracker do
       expect(@stat_tracker.win_totals_by_season("20122013")).to eq({"3"=>0, "6"=>4})
     end
 
-    it "has total_games_played_by_season which returns a hash of team_ids => total games played" do 
+    it "has total_games_played_by_season which returns a hash of team_ids => total games played" do
       expect(@stat_tracker.total_games_played_by_season("20122013")).to eq({"3"=>5, "6"=>4})
     end
 
-    it "has team_percentage_wins_by_season which takes a team_id and season_id and returns a percentage" do 
+    it "has team_percentage_wins_by_season which takes a team_id and season_id and returns a percentage" do
       expect(@stat_tracker.team_percentage_wins_by_season("3", "20122013")).to eq 0.0
       expect(@stat_tracker.team_percentage_wins_by_season("6", "20122013")).to eq 1.0
     end
 
-    it "has team_percentage_wins_all_seasons which takes a team_id and returns a hash of season_ids => win percentage" do 
+    it "has team_percentage_wins_all_seasons which takes a team_id and returns a hash of season_ids => win percentage" do
       expect(@stat_tracker.team_percentage_wins_all_seasons("3")).to eq({"20122013"=>0.0})
       expect(@stat_tracker.team_percentage_wins_all_seasons("6")).to eq({"20122013"=>1.0})
     end
@@ -205,12 +205,12 @@ RSpec.describe StatTracker do
     #these two tests need refactoring - we need better dummy files with multiple seasons
     #didn't want to change now since we're all using the same files to write our tests
     #but definitely should happen during iteration 3
-    
-    it "has best_season which takes a team_id and returns the season_id with highest win percentage" do 
+
+    it "has best_season which takes a team_id and returns the season_id with highest win percentage" do
       expect(@stat_tracker.best_season("3")).to eq "20122013"
     end
 
-    it "has best_season which takes a team_id and returns the season_id with lowestwin percentage" do 
+    it "has best_season which takes a team_id and returns the season_id with lowestwin percentage" do
       expect(@stat_tracker.worst_season("3")).to eq "20122013"
     end
   end
