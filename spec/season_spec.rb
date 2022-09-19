c17af4e74207d5cf12c231768840bdf47a29c9a9
Emily Port <emilylport@gmail.com>
2022-09-19 10:04:42 -0600

    create a season class and spec file, season is a child of games

--- spec/season_spec.rb
+++ spec/season_spec.rb
@@ -2,8 +2,18 @@ require 'spec_helper.rb'
 require './lib/season'
 require 'csv'
 
-# describe "season helper methods" do
-#   it 'has games_by_season which returns an array of games_ids' do
-#     expect(@games.games_by_season('20122013')).to eq ['2012030221', '2012030222', '2012030223', '2012030224', '2012030225', '2012030311', '2012030312', '2012030313', '2012030314']
-#   end
-# end
+RSpec.describe Season do 
+  before(:each) do 
+    dummy_game_path = './data/dummy_games.csv'
+    games_data = CSV.read dummy_game_path, headers:true 
+    @season = Season.new(games_data, "20122013")
+  end
+
+  describe 'list of game ids' do 
+    it 'returns a list of game ids for this season' do 
+      expect(@season.list_of_game_ids).to eq(["2012030221", "2012030222", "2012030223", "2012030224", "2012030225", "2012030311", "2012030312", "2012030313", "2012030314"])
+    end
+  end
+
+end
+
