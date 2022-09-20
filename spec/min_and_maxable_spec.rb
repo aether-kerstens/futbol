6a8a9b4dcf8d404a6ea609c59b6661d26a0275a4
Emily Port <emilylport@gmail.com>
2022-09-20 17:29:25 -0600

    add testing for the min_and_maxable module

--- spec/min_and_maxable_spec.rb
+++ spec/min_and_maxable_spec.rb
@@ -1,8 +1,16 @@
-# require './lib/min_and_maxable'
-#
-# RSpec.describe MinAndMaxable do
-#   it 'key_at_max takes hash and returns a key at max value' do
-#     hash = {"TeamAmanda" => 1, "TeamEmily" => 2, "TeamPatricia" => 6}
-#     expect(key_at_max(hash)).to eq("TeamPatricia")
-#   end
-# end
+require './lib/min_and_maxable'
+require './lib/stat_tracker'
+
+RSpec.describe MinAndMaxable do
+  it 'key_at_max takes hash and returns a key at max value' do
+    stat_tracker = StatTracker.new(1,2,3)
+    hash = {"TeamAmanda" => 1, "TeamEmily" => 2, "TeamPatricia" => 6}
+    expect(stat_tracker.key_at_max(hash)).to eq("TeamPatricia")
+  end
+
+  it 'key_at_min takes hash and returns a key at min value' do
+    stat_tracker = StatTracker.new(1,2,3)
+    hash = {"TeamAmanda" => 1, "TeamEmily" => 2, "TeamPatricia" => 6}
+    expect(stat_tracker.key_at_min(hash)).to eq("TeamAmanda")
+  end
+end
